3118c9c389b023f15d81c3cbe33a7b3a8bc5a492
mejta <daniel@mejta.net>
2022-02-15 21:15:02 +0100

    Add link field type

--- assets/components/GutenbergBlock.js
+++ assets/components/GutenbergBlock.js
@@ -13,8 +13,6 @@ const EDIT_VIEW = 'EDIT_VIEW';
 const GutenbergBlock = (props) => {
 	const { appContext, attributes, isSelected } = props;
 
-	console.log(attributes);
-
 	const [view, setView] = useState(DESKTOP_VIEW);
 
 	useEffect(() => {
--- build/wpify-custom-blocks.asset.php
+++ build/wpify-custom-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '745011d2dd1aed3ce401a6146ebce98f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'a3c9fa1158b31a27e378579d5f97bdc2');
