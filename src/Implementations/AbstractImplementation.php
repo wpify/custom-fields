beb3693893873dade07ac930efac4f283c71a778
mejta <daniel@mejta.net>
2023-12-04 15:40:39 +0100

    Fix post field type for gutenberg

--- src/Implementations/AbstractImplementation.php
+++ src/Implementations/AbstractImplementation.php
@@ -352,8 +352,6 @@ abstract class AbstractImplementation {
 			case 'number':
 				return 'number';
 			case 'attachment':
-			case 'post':
-				return 'integer';
 			case 'multi_attachment':
 			case 'multi_group':
 			case 'multi_post':
