c3a64b34a1d750d019746983ff1cac0a5820e396
mejta <daniel@mejta.net>
2023-12-12 21:22:12 +0100

    Add wcf_field_props filter

--- build/wpify-custom-blocks.asset.php
+++ build/wpify-custom-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'f7c813e0e1c0b9a2733a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '25509097c6ef65e58e8b');
--- build/wpify-custom-fields.asset.php
+++ build/wpify-custom-fields.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => '92b4346ea14254cd37a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => 'a5db2992687b1a6482af');
