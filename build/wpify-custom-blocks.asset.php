327546f78dd4751737606401e100f0d28498de97
mejta <mejta@users.noreply.github.com>
2023-12-12 17:32:46 +0000

    Add built assets for refs/heads/master

---
+++ build/wpify-custom-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '80df6283e31580f6dd59');
