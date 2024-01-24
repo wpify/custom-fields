0b3fe26bf6531816c86ad20661bf7e198076caee
mejta <daniel@mejta.net>
2024-01-24 09:37:09 +0100

    Fix WYSIWYG editor toolbar

--- build/wpify-custom-blocks.asset.php
+++ build/wpify-custom-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'a9d7f03d73cd0d861f9f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'f07f6e07a400933275b5');
--- build/wpify-custom-fields.asset.php
+++ build/wpify-custom-fields.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => 'd7ab360b694e9df5a128');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-date', 'wp-hooks', 'wp-i18n'), 'version' => 'c879c350a27461882ebb');
