a03fc66e4056d0ea1ca48ad83654922ded81b42d
mejta <daniel@mejta.net>
2024-09-27 12:03:17 +0200

    next

--- build/wpify-custom-fields.asset.php
+++ build/wpify-custom-fields.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '7429d6273004027aab95');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '36c89860861e9f9c9056');
