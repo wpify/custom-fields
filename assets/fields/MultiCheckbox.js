bc3cd6dbb4d08d3f9e98405cc3135865a7115398
mejta <daniel@mejta.net>
2024-10-25 17:22:24 +0200

    Fix

--- assets/fields/MultiCheckbox.js
+++ assets/fields/MultiCheckbox.js
@@ -1,7 +1,7 @@
-import { useCallback } from 'react';
-import { addFilter } from '@wordpress/hooks';
-import { checkValidityMultiBooleanType } from '@/helpers/validators';
-import clsx from 'clsx';
+import { useCallback } from 'react'
+import { addFilter } from '@wordpress/hooks'
+import { checkValidityMultiBooleanType } from '@/helpers/validators'
+import clsx from 'clsx'
 
 function MultiCheckbox ({
   id,
@@ -13,40 +13,39 @@ function MultiCheckbox ({
   className,
 }) {
   const handleChange = useCallback(optionValue => event => {
-    const nextValue = Array.isArray(value) ? [ ...value ] : [];
+    const nextValue = Array.isArray(value) ? [...value] : []
 
     if (event.target.checked) {
-      nextValue.push(optionValue);
+      nextValue.push(optionValue)
     } else {
-      nextValue.splice(nextValue.indexOf(optionValue), 1);
+      nextValue.splice(nextValue.indexOf(optionValue), 1)
     }
 
-    onChange(nextValue.filter((value, index, array) => array.indexOf(value) === index));
-  }, [onChange, value]);
+    onChange(nextValue.filter((value, index, array) => array.indexOf(value) === index))
+  }, [onChange, value])
 
-  return (
-    <div className={clsx('wpifycf-field-multi-checkbox', `wpifycf-field-multi-checkbox--${id}`, className)}>
-      {options.map(option => (
-        <div className={`wpifycf-field-multi-checkbox__item wpifycf-field-multi-checkbox__item--${option.value}`} key={option.value}>
-          <input
-            type="checkbox"
-            id={`${htmlId}-${option.value}`}
-            onChange={handleChange(option.value)}
-            checked={Array.isArray(value) ? value.includes(option.value) : false}
-            {...attributes}
-          />
-          <label
-            className="wpifycf-field-multi-checkbox__label"
-            htmlFor={`${htmlId}-${option.value}`}
-          >
-            {option.label}
-          </label>
-        </div>
-      ))}
-    </div>
-  );
+  return (<div className={clsx('wpifycf-field-multi-checkbox', `wpifycf-field-multi-checkbox--${id}`, className)}>
+    {options.map(option => (
+      <div
+        className={`wpifycf-field-multi-checkbox__item wpifycf-field-multi-checkbox__item--${option.value}`}
+        key={option.value}>
+        <input
+          type="checkbox"
+          id={`${htmlId}-${option.value}`}
+          onChange={handleChange(option.value)}
+          checked={Array.isArray(value) ? value.includes(option.value) : false}
+          {...attributes}
+        />
+        <label
+          className="wpifycf-field-multi-checkbox__label"
+          htmlFor={`${htmlId}-${option.value}`}
+          dangerouslySetInnerHTML={{ __html: option.label }}
+        />
+      </div>
+    ))}
+  </div>)
 }
 
-MultiCheckbox.checkValidity = checkValidityMultiBooleanType;
+MultiCheckbox.checkValidity = checkValidityMultiBooleanType
 
-addFilter('wpifycf_field_multi_checkbox', 'wpify_custom_fields', () => MultiCheckbox);
+addFilter('wpifycf_field_multi_checkbox', 'wpify_custom_fields', () => MultiCheckbox)
--- assets/fields/MultiToggle.js
+++ assets/fields/MultiToggle.js
@@ -1,8 +1,8 @@
-import { useCallback } from 'react';
-import { addFilter } from '@wordpress/hooks';
-import { ToggleControl } from '@wordpress/components';
-import { checkValidityMultiBooleanType } from '@/helpers/validators';
-import clsx from 'clsx';
+import { useCallback } from 'react'
+import { addFilter } from '@wordpress/hooks'
+import { ToggleControl } from '@wordpress/components'
+import { checkValidityMultiBooleanType } from '@/helpers/validators'
+import clsx from 'clsx'
 
 function MultiToggle ({
   id,
@@ -13,33 +13,36 @@ function MultiToggle ({
   className,
 }) {
   const handleChange = useCallback(optionValue => checked => {
-    const nextValue = Array.isArray(value) ? [ ...value ] : [];
+    const nextValue = Array.isArray(value) ? [...value] : []
 
     if (checked) {
-      nextValue.push(optionValue);
+      nextValue.push(optionValue)
     } else {
-      nextValue.splice(nextValue.indexOf(optionValue), 1);
+      nextValue.splice(nextValue.indexOf(optionValue), 1)
     }
 
-    onChange(nextValue.filter((value, index, array) => array.indexOf(value) === index));
-  }, [onChange, value]);
+    onChange(nextValue.filter((value, index, array) => array.indexOf(value) === index))
+  }, [onChange, value])
 
   return (
     <div className={clsx('wpifycf-field-multi-toggle', `wpifycf-field-multi-toggle--${id}`, className)}>
       {options.map(option => (
-        <div className={`wpifycf-field-multi-toggle__item wpifycf-field-multi-checkbox__item--${option.value}`} key={option.value}>
+        <div
+          className={`wpifycf-field-multi-toggle__item wpifycf-field-multi-checkbox__item--${option.value}`}
+          key={option.value}
+        >
           <ToggleControl
             id={`${htmlId}-${option.value}`}
             onChange={handleChange(option.value)}
             checked={Array.isArray(value) ? value.includes(option.value) : false}
-            label={option.label}
+            label={<span dangerouslySetInnerHTML={{ __html: option.label }}/>}
           />
         </div>
       ))}
     </div>
-  );
+  )
 }
 
-MultiToggle.checkValidity = checkValidityMultiBooleanType;
+MultiToggle.checkValidity = checkValidityMultiBooleanType
 
-addFilter('wpifycf_field_multi_toggle', 'wpify_custom_fields', () => MultiToggle);
+addFilter('wpifycf_field_multi_toggle', 'wpify_custom_fields', () => MultiToggle)
