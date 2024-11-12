08ca9c12f42dee43336b532f12e31fa924e60aed
mejta <daniel@mejta.net>
2024-11-12 18:52:34 +0100

    Fix

--- build/wpify-custom-fields.asset.php
+++ build/wpify-custom-fields.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'e2490e972d1ebca6b53a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'fbe075f9d2e4398483dc');
