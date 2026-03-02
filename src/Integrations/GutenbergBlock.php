3f5db4eb5fc9aa3103630e3ec02a1fc1875ed5b3
Daniel Mejta <daniel@mejta.net>
2026-03-02 14:22:24 +0100

    Do not render block in rest requests

--- src/Integrations/GutenbergBlock.php
+++ src/Integrations/GutenbergBlock.php
@@ -437,7 +437,7 @@ class GutenbergBlock extends BaseIntegration {
 	 * @return string The rendered block content.
 	 */
 	public function render( array $attributes, string $content, WP_Block $block ): string {
-		if ( ( defined( 'REST_REQUEST' ) && REST_REQUEST ) || null === $this->render_callback ) {
+		if ( ( defined( 'REST_REQUEST' ) && REST_REQUEST ) || wp_doing_ajax() || null === $this->render_callback ) {
 			return $content;
 		}
 
